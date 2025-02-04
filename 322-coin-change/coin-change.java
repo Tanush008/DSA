6593bc8a4bf3479899ab982199202d4f2bf444e3
Tanush008 <149870803+Tanush008@users.noreply.github.com>
2025-02-04 19:53:02 +0530

    Time: 29 ms (25.41%) | Memory: 45.5 MB (15.27%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,18 +1,6 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        // int[][] dp = new int[n][amount + 1];
-        // for (int T = 0; T <= amount; T++) {
-        // dp[0][T] = (T % coins[0] == 0) ? 1 : 0;
-        // }
-        // for (int i = 1; i < n; i++) {
-        // for (int t = 0; t <=amount; t++) {
-        // int notaken = dp[i - 1][t];
-        // int taken = (t >= coins[i]) ? dp[i][t - coins[i]] : 0;
-        // dp[i][t] = taken + notaken;
-        // }
-        // }
-        // return dp[n - 1][amount];
         int[][] dp = new int[n][amount + 1];
         for (int[] i : dp) {
             Arrays.fill(i, -1);
@@ -25,22 +13,24 @@ class Solution {
         }
     }
 
-    public int countToTarget(int[] arr, int i, int T, int dp[][]) {
-        if (i == 0) {
-            if (T % arr[0] == 0) {
-                return T / arr[0];
-            } else {
-                return (int) (Math.pow(10, 9));
+    public int countToTarget(int[] coins, int index, int Target, int dp[][]) {
+        if (index == 0) {
+            if (Target % coins[index] == 0) {
+                dp[index][Target] = Target / coins[index];
+                return Target / coins[index];
             }
+            dp[index][Target] = Target / coins[index];
+            return (int) (1e9);
         }
-        if (dp[i][T] != -1) {
-            return dp[i][T];
+        if (dp[index][Target] != -1) {
+            return dp[index][Target];
         }
-        int notaken = 0 + countToTarget(arr, i - 1, T, dp);
-        int taken = (int) (Math.pow(10, 9));
-        if (T >= arr[i]) {
-            taken = 1 + countToTarget(arr, i, T - arr[i], dp);
+        int pick = (int) (1e9);
+        if (Target >= coins[index]) {
+            pick = 1 + countToTarget(coins, index, Target - coins[index], dp);
         }
-        return dp[i][T] = Math.min(taken, notaken);
+        int noPick = countToTarget(coins, index - 1, Target, dp);
+        dp[index][Target] = Math.min(pick, noPick);
+        return dp[index][Target];
     }
 }
