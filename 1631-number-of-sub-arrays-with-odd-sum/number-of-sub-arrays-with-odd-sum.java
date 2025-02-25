d04476a49aa928602d60eb47377fd5790a839188
Tanush008 <149870803+Tanush008@users.noreply.github.com>
2025-02-25 11:21:21 +0530

    Time: 7 ms (83.08%) | Memory: 58.5 MB (18.44%) - LeetSync

--- 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.java
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.java
@@ -6,8 +6,8 @@ class Solution {
         int evenCount = 1;
         int oddCount = 0;
         int count = 0;
-        for (int i = 0; i < n; i++) {
-            prefixSum += arr[i];
+        for (int i:arr) {
+            prefixSum += i;
             if (prefixSum % 2 == 0) {
                 count += oddCount;
                 evenCount++;
