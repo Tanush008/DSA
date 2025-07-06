04b55a5b97ad550c79edd7372e91b1aeae98128f
Tanush008 <149870803+Tanush008@users.noreply.github.com>
2025-07-06 19:07:57 +0530

    Time: 1 ms (83.57%) | Memory: 41.8 MB (96.07%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -5,6 +5,7 @@ class Solution {
             List<Integer> temp = new ArrayList<>();
             for (int j = 1; j <= i; j++) {
                 temp.add(pascal(i - 1, j - 1));
+
             }
             res.add(temp);
         }
