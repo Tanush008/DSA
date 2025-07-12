a7358c0afc5cabbe0f10c3a54b471f274f1f75f9
Tanush008 <149870803+Tanush008@users.noreply.github.com>
2025-07-12 23:47:02 +0530

    Time: 1 ms (99.89%) | Memory: 48.8 MB (35.98%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -29,7 +29,7 @@ public class Solution {
         }
         while (headA != null) {
             if (headA == headB) {
-                return headA;
+                return headB;
             }
             headB = headB.next;
             headA = headA.next;
