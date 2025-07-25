01d1829e5cb254881f4fbe0e9780823e7f432d72
Tanush008 <149870803+Tanush008@users.noreply.github.com>
2025-07-25 23:29:18 +0530

    Time: 8 ms (50.41%) | Memory: 57.2 MB (30.08%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -29,11 +29,9 @@ class Solution {
         int left = 0;
         int right = s.length() - 1;
         while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
+            if (s.charAt(left++) != s.charAt(right--)) {
                 return false;
             }
-            left++;
-            right--;
         }
         return true;
     }
