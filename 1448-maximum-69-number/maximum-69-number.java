29bee21590c0f1bf8b48ec70e63f90ec00b09ca0
Tanush008 <149870803+Tanush008@users.noreply.github.com>
2025-08-17 01:06:51 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (56.62%) - LeetSync

---
+++ 1448-maximum-69-number/maximum-69-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maximum69Number(int num) {
+        if (num == 9999) {
+            return num;
+        }
+        char[] digit = String.valueOf(num).toCharArray();
+        for (int i = 0; i < digit.length; i++) {
+            if (digit[i] == '6') {
+                digit[i] = '9';
+                break;
+            }
+        }
+        return Integer.parseInt(new String(digit));
+    }
+}
