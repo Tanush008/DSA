0161a248ca26e13d28885f35905146a58d9a4665
Tanush008 <149870803+Tanush008@users.noreply.github.com>
2025-08-22 22:02:52 +0530

    Time: 4 ms (76.06%) | Memory: 43.9 MB (40.30%) - LeetSync

--- 867-new-21-game/new-21-game.java
+++ 867-new-21-game/new-21-game.java
@@ -1,9 +1,9 @@
 class Solution {
     public double new21Game(int n, int k, int maxPts) {
         double[] probSum = new double[n+1];
-        probSum[0] = 1.0;
         double currSum = (k == 0) ? 0.0 : 1.0;
-        for (int i = 1; i <= n; i++) {
+        probSum[0] = 1.0;
+        for (int i = 1; i <=n; i++) {
             probSum[i] = currSum / maxPts;
             if (i < k) {
                 currSum += probSum[i];
@@ -13,7 +13,7 @@ class Solution {
             }
         }
         double res = 0.0;
-        for (int i = k; i <= n; i++) {
+        for (int i = k; i<=n; i++) {
             res += probSum[i];
         }
         return res;
