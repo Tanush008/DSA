7486d755bd470e5b4625ae5d74711ce085bb010d
Tanush008 <149870803+Tanush008@users.noreply.github.com>
2025-08-23 11:11:50 +0530

    Time: 16 ms (46.95%) | Memory: 44.5 MB (79.83%) - LeetSync

--- 679-24-game/24-game.java
+++ 679-24-game/24-game.java
@@ -1,46 +1,48 @@
 class Solution {
     public boolean judgePoint24(int[] cards) {
-        // int n = card.length;
         List<Double> list = new ArrayList<>();
-        for (int card : cards) {
-            list.add((double) card);
+        for(int card:cards){
+            list.add((double)card);
         }
         return solve(list);
     }
 
-    public boolean solve(List<Double> num) {
-        if (num.size() == 1) {
-            return Math.abs(num.get(0) - 24.0) < 1e-6;
+    public boolean solve(List<Double> list) {
+        // List<Integer>list=new ArrayList<>(cards);
+        if (list.size() == 1) {
+            return Math.abs(list.get(0) - 24.0) < 1e-6;
         }
-        int n = num.size();
+        int n = list.size();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (i == j) {
                     continue;
                 }
-                List<Double> next = new ArrayList<>();
+                List<Double> nxt = new ArrayList<>();
                 for (int k = 0; k < n; k++) {
                     if (k != i && k != j) {
-                        next.add(num.get(k));
+                        nxt.add(list.get(k));
                     }
                 }
-                double a = num.get(i), b = num.get(j);
+                double a = list.get(i);
+                double b = list.get(j);
                 List<Double> res = new ArrayList<>();
                 res.add(a + b);
-                res.add(a - b);
                 res.add(b - a);
-                res.add(a * b);
-                if (Math.abs(b) > 1e-6) {
-                    res.add(a / b);
-                }
-                if (Math.abs(a) > 1e-6) {
+                res.add(a - b);
+                if (Math.abs(a) > 0.0) {
                     res.add(b / a);
                 }
+                if (Math.abs(b) > 1.0) {
+                    res.add(a / b);
+                }
+                res.add(a*b);
                 for (double val : res) {
-                    next.add(val);
-                    if (solve(next))
+                    nxt.add(val);
+                    if (solve(nxt)) {
                         return true;
-                    next.remove(next.size() - 1);
+                    }
+                    nxt.remove(nxt.size() - 1);
                 }
             }
         }
